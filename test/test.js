bd145a326c34f4d83a099840cd77d577de48e8be
Eliran Pe'er <eliran.peer@forter.com>
2017-10-24 22:47:52 +0300

    add lowercase name test

--- test/test.js
+++ test/test.js
@@ -1,15 +1,16 @@
 const assert = require('assert');
 const fs = require('fs-extra');
 const Validator = require('jsonschema').Validator;
+const path = require('path');
+
+const files = fs.readdirSync('./collection');
 
 describe('name files', () => {
     it('should contain valid JSON', done => {
         (async () => {
-            const files = await fs.readdir('./collection');
-
             for (const fileName of files) {
                 try {
-                    const contents = await fs.readFile('./collection/' + fileName);
+                    const contents = fs.readFileSync('./collection/' + fileName);
                     JSON.parse(contents);
                 } catch (err) {
                     done(`Error in name file "${fileName}":\n${err}`);
@@ -20,25 +21,31 @@ describe('name files', () => {
         })();
     });
 
-    it('should not have duplicate names', done => {
-        (async () => {
-            const files = await fs.readdir('./collection');
-            const names = [];
+    it('should contain a lowercase name, same as the filename', done => {
+        for (const fileName of files) {
+            const contents = fs.readFileSync('./collection/' + fileName);
+            const json = JSON.parse(contents);
+            assert.ok(json.name === json.name.toLowerCase(), 'name is not lowercase');
+            assert.ok(json.name === path.basename(fileName, '.json'), 'fileName should match the name');
+        }
 
-            for (const fileName of files) {
-                const contents = await fs.readFile('./collection/' + fileName);
-                var json = JSON.parse(contents);
-                names.push(json.name);
-            }
-
-            var isDuplicate = (new Set(names).size !== names.length);
-            assert.equal(isDuplicate, false);
-            done();
-        })();
+        done();
     });
-});
 
-describe('schema specifications', () => {
+    it('should not have duplicate names', done => {
+        const names = [];
+
+        for (const fileName of files) {
+            const contents = fs.readFileSync('./collection/' + fileName);
+            var json = JSON.parse(contents);
+            names.push(json.name);
+        }
+
+        var isDuplicate = (new Set(names).size !== names.length);
+        assert.equal(isDuplicate, false);
+        done();
+    });
+
     it('should have correct structure', done => {
         const jsonValidator = new Validator();
 
@@ -67,24 +74,21 @@ describe('schema specifications', () => {
             }
         };
 
-        (async () => {
-            const files = await fs.readdir('./collection');
-            for (const fileName of files) {
-                try {
-                    const contents = await fs.readFile('./collection/' + fileName);
-                    const json = JSON.parse(contents);
+        for (const fileName of files) {
+            try {
+                const contents = fs.readFileSync('./collection/' + fileName);
+                const json = JSON.parse(contents);
 
-                    // Validate all the name files file against the defined schema above
-                    const validationResult = jsonValidator.validate(json, nameFileSchema, { throwError: true });
+                // Validate all the name files file against the defined schema above
+                const validationResult = jsonValidator.validate(json, nameFileSchema, { throwError: true });
 
-                    // Ensure there are no validation errors
-                    assert.equal(validationResult.errors.length, 0);
-                } catch (err) {
-                    done(`Error in name file "${fileName}":\n${err}`);
-                }
+                // Ensure there are no validation errors
+                assert.equal(validationResult.errors.length, 0);
+            } catch (err) {
+                done(`Error in name file "${fileName}":\n${err}`);
             }
+        }
 
-            done();
-        })();
+        done();
     });
 });
