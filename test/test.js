dd6a71d47da6bf9fd898b1d7ad85d2f708d6a978
Chris Fitzmorris <cfitz93@gmail.com>
2017-10-23 21:55:11 -0500

    Add test for UTF-8 encoding

--- test/test.js
+++ test/test.js
@@ -1,7 +1,23 @@
 var assert = require('assert');
+var fs = require('fs');
 
 describe('names.json', function () {
-    it('should contain valid JSON', function () {
-        var names = require.main.require('names.json');
+    it('should contain valid JSON', function (done) {
+        fs.readFile("./names.json", function (err, text) {
+            if (err) done(err);
+            var json = JSON.parse(text, 'utf8');
+            done();
+        });
+    });
+
+    it('should be UTF-8 encoded', function (done) {
+        var chardet = require('chardet');
+        
+        chardet.detectFile('./names.json', function (err, encoding) {
+            if (err) done(err);
+            
+            assert.equal(encoding, 'UTF-8');
+            done();
+        });
     });
 });
