025b0c48f69085a3745bb85575a587711fe9b4c6
Eliran Pe'er <eliran013@gmail.com>
2017-10-28 00:34:41 +0300

    Add basic API to query names  (#193)
    * adding deployment script
    
    * add deploy to travis.yml
    
    * travis config
    
    * initial commit - api
    
    * add api

---
+++ api/routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const utils = require('./utils');
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.send({ state: 'running' });
+});
+
+router.get('/:name', (req, res, next) => {
+  utils.findName(req.params.name)
+    .then(result => res.status(200).send(result).end())
+    .catch(err => res.status(404).send({ err: true, debug: err }));
+});
+
+module.exports = router;
