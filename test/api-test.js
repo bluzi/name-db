3504651fc09271c4bc286ed7c8f839271d245be8
srpalomino <salvatpalvat@gmail.com>
2017-11-10 23:23:01 -0500

    The first test case of /:name has passing tests

--- test/api-test.js
+++ test/api-test.js
@@ -9,3 +9,44 @@ const err = true;
 
 app.use('/', router);
 const server = app.listen(5000);
+
+
+
+
+
+describe('/:name', () => {
+  it('sends correct name data with a 200 resp status when querying names existing in the db', (done) => {
+    request(server)
+      .get('/jack')
+      .expect(200)
+      .expect(jackTest);
+
+    request(server)
+      .get('/mary')
+      .expect(200)
+      .expect(maryTest)
+      .end(done);
+  })
+  it('sends an err msg with a 404 resp status when querying names not existing in the db', (done) => {
+    done()
+  })
+})
+
+
+
+
+
+
+function jackTest(res) {
+  const name = res.body.name;
+  if (name !== jack) {
+    throw new Error("Response does not contain the correct name data");
+  }
+}
+
+function maryTest(res) {
+  const name = res.body.name;
+  if (name !== mary) {
+    throw new Error("Response does not contain the correct name data");
+  }
+}
